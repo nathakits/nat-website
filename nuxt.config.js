e0150657ef43334c58380cd3cbddd6400362081c
nathakit.sae-tan@cuinda.com <nathakit.sae-tan@cuinda.com>
2018-03-30 02:32:50 +0700

    add nuxt files and components

---
+++ nuxt.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+
+  head: {
+    titleTemplate: 'Nathakit Sae-Tan',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Meta description' }
+    ]
+  },
+
+  // modules: [
+  //   '@nuxt/pwa'
+  // ],
+
+  manifest: {
+    name: "Nathakit Sae-Tan",
+    description: "Nathakit Sae-Tan",
+    theme_color: "#38ea92",
+  },
+
+  css: [
+    '~/assets/css/main.styl'
+  ],
+
+  build: {
+    vendor: [
+      'vue-progressive-image',
+    ],
+    extend (config, { isDev, isClient, isServer }) {
+      if (isDev || isClient || isServer) {
+        const urlLoader = config.module.rules.find((rule) => rule.loader === 'url-loader')
+        urlLoader.options.limit = 2000 // 2kb
+      }
+    }
+  },
+
+  plugins: [
+    { src: "~/plugins/vue-progressive-image.js", ssr: false }
+  ],
+
+  generate: {
+    dir: 'dist'
+  }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
-<template>
-  <h1>Hello world!</h1>
+<template lang='pug'>
+div(id='home')
+
+	div(style="position:absolute;z-index:10;")
+		h1 NST
+		<p></br></p>
+		<p>I am a designer, coder, and entrepeneur.</p>
+		<p></br></p>
+		<p>I am a designer at <a href="https://pupil-labs.com">Pupil Labs</a>.</p>
+		<p></br></p>
+		<p>I am currently <a href="https://time.is/UTC+7">UTC+7</a>.</p><p>(Bangkok, Thailand).</p>
+		<p></br></p>
+		<p><a href="https://github.com/nathakits">Github</a> <a href="https://www.linkedin.com/in/nathakit-saetan-b4ab8379/">Linkedin</a> <a href="mailto:nat@pupil-labs.com">Email</a> <a href="https://www.facebook.com/moonkkin/">Facebook</a> </p>
+
+	div.bg-image
+		no-ssr
+			progressive-background(:src="require('~/assets/imgs/bucky_lv220.jpg')")
 </template>
