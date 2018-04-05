429ded0402be8e3a1d1285f0b9a37d2b6d0fddd9
nathakit.sae-tan@cuinda.com <nathakit.sae-tan@cuinda.com>
2018-04-06 01:31:20 +0700

    update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,12 +6,23 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Meta description' }
+    ],
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' },
+      { rel: 'styleshhet' href: 'https://fonts.googleapis.com/css?family=Open+Sans'}
+      { rel: 'stylesheet' href: 'https://fonts.googleapis.com/icon?family=Material+Icons'},
     ]
   },
 
-  // modules: [
-  //   '@nuxt/pwa'
-  // ],
+  modules: [
+		'@nuxtjs/pwa'
+	],
+
+  dev: (process.env.NODE_ENV !== 'production'),
+
+  env: {
+    baseUrl: process.env.BASE_URL || 'http://localhost:3000'
+  },
 
   manifest: {
     name: "Nathakit Sae-Tan",
