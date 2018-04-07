e86235cf2650d1f127b668fb36d371d3e3d5ab98
nathakit.sae-tan@cuinda.com <nathakit.sae-tan@cuinda.com>
2018-04-07 18:19:22 +0700

    update footer

--- components/footer.vue
+++ components/footer.vue
@@ -6,20 +6,23 @@ footer
 			i.material-icons copyright
 
 		div
-			a.grid.alignItems-center(href="mailto:nat@pupil-labs.com")
+			a.grid.alignItems-center(href="mailto:nat@pupil-labs.com" title="Email")
 				i.material-icons email
 				h3.email nat@pupil-labs.com
 
 		div.grid.alignItems-center.icon-container
-
-			a(href="https://www.pinterest.com/nathakit/" title="Pinterest")
-				img.icon(src="~/assets/icons/pinterest.svg")
 
 			a(href="https://github.com/nathakits" title="Github")
 				img.icon(src="~/assets/icons/github.svg")
 
 			a(href="https://www.linkedin.com/in/nathakit-saetan-b4ab8379/" title="Linkedin")
 				img.icon(src="~/assets/icons/linkedin.svg")
+
+			a(href="https://www.pinterest.com/nathakit/" title="Pinterest")
+				img.icon(src="~/assets/icons/pinterest.svg")
+
+			a(href="https://twitter.com/nathakit_tan" title="Twitter")
+				img.icon(src="~/assets/icons/twitter.svg")
 
 			a(href="https://www.facebook.com/moonkkin/" title="Facebook")
 				img.icon(src="~/assets/icons/facebook.svg")
