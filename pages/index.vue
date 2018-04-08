3ba794931fdde6300bba92800a84fec4122c8087
nathakit.sae-tan@cuinda.com <nathakit.sae-tan@cuinda.com>
2018-04-08 22:08:34 +0700

    update index page

--- pages/index.vue
+++ pages/index.vue
@@ -2,22 +2,22 @@
 
 main(id='home')
 	div.bg-image
-		no-ssr
-			//- TODO
-			//- update background image with new render
-			//- remove rings and change sjy color
-			progressive-background(:src="require('~/assets/imgs/bucky_lv220.jpg')")
+		figure
+			no-ssr
+				//- TODO
+				//- update background image with new render
+				//- remove rings and change sjy color
+				div.bg-full-wh
+					progressive-background(:src="require('~/assets/imgs/bucky_lv220.jpg')")
 
-	div.content-container
-		div.grid.column.intro-container.bold
-			h1 Designer 
-			h1 Coder
-			h1 Entrepreneur
-		div.grid.column.intro-container.bold
-			h1 Currently at 
-				a(href="https://pupil-labs.com" title=" Accessible open source eye tracking platform").underline Pupil Labs
-
-
+		div.content-container
+			div.grid.column.intro-container.bold
+				h1 Designer 
+				h1 Coder
+				h1 Entrepreneur
+			div.grid.column.intro-container.bold
+				h1 Currently at 
+					a(href="https://pupil-labs.com" title=" Accessible open source eye tracking platform").underline Pupil Labs
 
 </template>
 
