f130ee61d8cb1851f59fa0c15ce0ac53f0f5b63e
Nathakit Sae-Tan <nathakit.sae-tan@cuinda.com>
2018-04-16 00:22:19 +0700

    add nav links

--- components/header.vue
+++ components/header.vue
@@ -1,9 +1,20 @@
 <template lang="pug">
 
 header
-	div.grid.justifyContent-between.header-container
-		a.logo-link(href="/")
+	div.grid.justifyContent-between.header-container.alignItems-center
+		nuxt-link.logo-link(to="/")
 			h1.logotype(title="Nathakit Sae-Tan") NST
 
-</template>
+		//- add width to nav-links for multiple links
+		div.grid.justifyContent-between.nav-links
+			nuxt-link.logo-link(to="/info")
+				h4 Info
+			//- placeholder
+			//- nuxt-link.logo-link(to="/info")
+			//- 	h4 Info
+			//- nuxt-link.logo-link(to="/info")
+			//- 	h4 Info
+			//- nuxt-link.logo-link(to="/info")
+			//- 	h4 Info
 
+</template>
