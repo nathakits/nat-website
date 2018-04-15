5131a418962c383be958ba1bb728da8df55849ef
Nathakit Sae-Tan <nathakit.sae-tan@cuinda.com>
2018-04-16 00:19:47 +0700

    drafting new info page

---
+++ pages/info.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+	
+	main.content-container(id="info")
+		div 
+			p He is a visionary whose creativity and innovation spans multiple platforms. Utilizing many practices and disciplinaries to create something elegant and unique. 
+
+			p His ethos is driven by his desire to learn and be uncomfortable. He continously seeks to apply his foundation in architecture and design to new mediums. The results of these labours often take unexpected forms, be it commercials, installations, short films, title sequences, motion graphics, software applications, web front-end development and user interface / user experience.
+
+			p This fascination with multiple disciplinaries in design, computer and science informs in many types of interests ranging from automation, user centric design and hyper realistic graphics.
+
+			p work often centers around themes of identity, metaphysics, and the human connection to technology.
+			
+			p Available For Hire.
+
+</template>
