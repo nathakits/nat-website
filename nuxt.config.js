b24c163202f4546b95306891169711e9c434123e
nathakits <nathakit.tan@gmail.com>
2020-03-21 16:58:50 +0700

    lint and fix errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,31 @@
 export default {
-  mode: 'spa',
+  mode: "spa",
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/icon?family=Material+Icons"
+      }
     ]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
@@ -30,22 +33,20 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [
-    { src: '~plugins/ga.js', mode: 'client' }
-  ],
+  plugins: [{ src: "~plugins/ga.js", mode: "client" }],
   /*
    ** Nuxt.js dev-modules
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    "@nuxtjs/tailwindcss"
   ],
   /*
    ** Nuxt.js modules
    */
-  modules: ['@nuxtjs/pwa'],
+  modules: ["@nuxtjs/pwa"],
   /*
    ** Build configuration
    */
@@ -53,6 +54,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) { }
+    // extend() { }
   }
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,41 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">new-web</h1>
-      <h2 class="subtitle">Personal website</h2>
+      <Logo />
+      <h1 class="title">
+        new-web
+      </h1>
+      <h2 class="subtitle">
+        Personal website
+      </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+          >GitHub</a
+        >
       </div>
     </div>
   </div>
 </template>
 
 <script>
-console.log(
-  '%c Designed and made by Nathakit Sae-Tan ',
-  'background: #222; color: #fff; font-size: 15px;'
-)
+import Logo from "~/components/Logo.vue";
 
-import Logo from '~/components/Logo.vue'
+console.log(
+  "%c Designed and made by Nathakit Sae-Tan ",
+  "background: #222; color: #fff; font-size: 15px;"
+);
 
 export default {
   components: {
     Logo
   }
-}
+};
 </script>
 
 <style>
@@ -43,8 +54,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
