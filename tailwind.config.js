322dbfdd97c45b023f5008793ce328dff8b019bc
nathakits <nathakit.tan@gmail.com>
2020-03-25 21:37:34 +0700

    add inter to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "Helvetica", "Arial", "sans-serif"]
+    }
+  },
   variants: {},
   plugins: []
 };
