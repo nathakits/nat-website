16b60bf9de3a26d9d4c59551fc4c7ff7f405ff1b
nathakits <nathakit.tan@gmail.com>
2020-03-25 21:52:17 +0700

    update flex and frid layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,38 @@
 <template lang="pug">
   div
     div.bg-gray-200.h-screen
-      div.container.mx-auto
-        div.pt-6
-          div.grid.grid-cols-2.grid-flow-row.gap-4.h-full
-            h1.text-3xl.font-bold Nat.
-            div.grid.grid-cols-4.gap-4
-              span.col-span-1.text-right Projects
-              span.col-span-1.text-right Photo
-              span.col-span-1.text-right Blog
-              span.col-span-1.text-right Info
-        div.py-20.calc-height
-          div.grid.grid-cols-8.grid-flow-row.gap-4.h-full
-            div.row-span-1.col-span-4
-              div.grid.grid-rows-2.h-full
-                div.grid-span-1
-                  p Nathakit Sae-Tan
-                  h1.text-4xl.font-bold Product designer
-                  h1.text-4xl.font-bold Developer
-                div.grid-span-1
-                  div.flex.justify-end.flex-col.h-full
-                    div.py-4
-                      p Social
-                    div.grid.grid-cols-4.gap-4
-                      span.col-span-1 Twiiter
-                      span.col-span-1 Github
-                      span.col-span-1 Dribbble
-                      span.col-span-1 LinkedIn
-            div.row-span-1.col-span-4
-              img.w-full.h-full.rounded.shadow-2xl(
-                src="https://tailwindcss.com/img/card-top.jpg"
-                alt="Sunset in the mountains"
-              )
+      div.container.mx-auto.h-full
+        div.p-10.flex.flex-col.justify-between.h-full
+          div
+            div.grid.grid-cols-2.grid-flow-row.gap-4.h-full
+              h1.text-3xl.font-bold Nat.
+              div.grid.grid-cols-4.gap-4
+                span.col-span-1.text-right Projects
+                span.col-span-1.text-right Photo
+                span.col-span-1.text-right Blog
+                span.col-span-1.text-right Info
+          div.calc-height.pb-8
+            div.grid.grid-cols-8.grid-rows-2.gap-4.h-full
+              div.row-span-2.col-span-4
+                div.grid.grid-rows-2.h-full
+                  div.grid-span-1
+                    p Nathakit Sae-Tan
+                    h1.text-4xl.font-bold Product designer
+                    h1.text-4xl.font-bold Developer
+                  div.grid-span-1
+                    div.flex.justify-end.flex-col.h-full
+                      div.py-4
+                        p Social
+                      div.grid.grid-cols-4.gap-4
+                        span.col-span-1 Twiiter
+                        span.col-span-1 Github
+                        span.col-span-1 Dribbble
+                        span.col-span-1 LinkedIn
+              div.row-span-2.col-span-4
+                img.w-full.h-full.rounded.shadow-2xl(
+                  src="https://tailwindcss.com/img/card-top.jpg"
+                  alt="Sunset in the mountains"
+                )
 
     //- div.container.mx-auto
     //-   div.div.pt-6
@@ -60,7 +61,7 @@ export default {
 }
 */
 .calc-height {
-  height: calc(100vh - 200px);
+  height: calc(100vh - 250px);
 }
 
 .flex-center {
