ee37704c361fa4420553fce4c9aeb5de6ef82f02
nathakits <nathakit.tan@gmail.com>
2020-03-26 22:31:54 +0700

    add page transition css to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,15 @@
     <nuxt />
   </div>
 </template>
+
+<style>
+page-enter-active,
+.page-leave-active {
+  transition: all 0.2s ease-out;
+}
+.page-enter,
+.page-leave-active {
+  opacity: 0;
+  transform-origin: 50% 50%;
+}
+</style>
