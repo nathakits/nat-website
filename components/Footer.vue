7e1f2bd4baeb12130f53354c810e5ef5c8168bf3
nathakits <nathakit.tan@gmail.com>
2020-03-27 22:21:26 +0700

    update footer layout - use flex for mobile screens

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,18 +3,18 @@
   div.container.mx-auto.h-full
     div.div.pt-6
       div.p-10
-        div.grid.grid-cols-8.gap-4
-          div.col-start-1.col-end-3
+        div.flex.flex-col(class="sm:grid sm:grid-cols-3 sm:gap-4 lg:grid-cols-8")
+          div.pb-4(class="sm:col-span-1 lg:col-start-1 lg:col-end-3")
             p.font-bold.text-gray-800 Copyright
             p.text-gray-700.text-sm © {{ setCurrentYear }} Nathakit Sae-Tan
-          div.col-start-4.col-end-6
+          div.pb-4(class="sm:col-span-1 lg:col-start-4 lg:col-end-6")
             p.font-bold.text-gray-800 Contact
             a.text-gray-700.text-sm(
               class="hover:underline hover:text-blue-500"
               href="mailto:nathakit.tan@gmail.com"
               title="email"
             ) nathakit.tan@gmail.com
-          div.col-start-7.col-end-9
+          div.pb-4(class="sm:col-span-1 lg:col-start-7 lg:col-end-9")
             p.font-bold.text-gray-800 Currently in
             a.text-gray-700.text-sm(
               class="hover:underline hover:text-blue-500"
