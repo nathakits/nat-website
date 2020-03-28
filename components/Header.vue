f8612f7fa45e1b61426e50e2a68e2f88eb83b067
nathakits <nathakit.tan@gmail.com>
2020-03-28 15:10:03 +0700

    add new header component

---
+++ components/Header.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  div.bg-gray-200
+    div.container.mx-auto.h-full
+      div.px-10.py-6
+        div.grid.gap-4.h-full.grid-cols-2
+          div
+            nuxt-link(to="/")
+              h1.text-3xl.font-bold.inline-block Nat.
+          div.grid-cols-4.gap-4.grid(class="sm:pt-2")
+            div.col-span-4.flex.items-center.justify-end(
+              class="md:pt-0 text-right"
+              v-for="link in headLinks"
+              :key="link.name"
+            )
+              nuxt-link(class="hover:underline hover:text-blue-500" :to="link.path") {{ link.name }}
+          //- div.flex.items-center.justify-end(class="sm:pt-2 sm:hidden")
+          //-   button.rounded-md.p-2.text-gray-500(class="hover:text-gray-700 hover:bg-gray-400 focus:outline-none")
+          //-     svg.h-6.w-6(stroke="currentColor" fill="none" viewBox="0 0 24 24")
+          //-       path(class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16")
+          //-       path(class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12")
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headLinks: [
+        // { name: "Projects", path: "/projects/" },
+        // { name: "Photo", path: "/photo/" },
+        // { name: "Blog", path: "/blog/" },
+        { name: "Info", path: "/info/" }
+      ]
+    };
+  }
+};
+</script>
