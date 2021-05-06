97bcfd663935fd9238df65e9399c866170de71e8
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-05-07 01:55:02 +0700

    add cv to header - update grid to flexbox

--- components/Header.vue
+++ components/Header.vue
@@ -6,13 +6,14 @@
           div
             nuxt-link(to="/")
               h1.text-3xl.font-bold.inline-block Nat.
-          div.grid-cols-4.gap-4.grid(class="sm:pt-2")
-            div.col-span-4.flex.items-center.justify-end(
-              class="md:pt-0 text-right"
-              v-for="link in headLinks"
+          div.d-flex.justify-end.text-right(class="sm:pt-2")
+            nuxt-link(
+              v-for="(link, index) in headLinks"
               :key="link.name"
-            )
-              nuxt-link(class="hover:underline hover:text-blue-500" :to="link.path") {{ link.name }}
+              class="hover:underline hover:text-blue-500"
+              :class="headLinks.length !== (index + 1) ? 'pr-6' : ''"
+              :to="link.path"
+            ) {{ link.name }}
           //- div.flex.items-center.justify-end(class="sm:pt-2 sm:hidden")
           //-   button.rounded-md.p-2.text-gray-500(class="hover:text-gray-700 hover:bg-gray-400 focus:outline-none")
           //-     svg.h-6.w-6(stroke="currentColor" fill="none" viewBox="0 0 24 24")
@@ -29,7 +30,8 @@ export default {
         // { name: "Projects", path: "/projects/" },
         // { name: "Photo", path: "/photo/" },
         // { name: "Blog", path: "/blog/" },
-        { name: "Info", path: "/info/" }
+        { name: "Info", path: "/info/" },
+        { name: "CV", path: "/cv/" }
       ]
     };
   }
