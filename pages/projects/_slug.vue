0a9d51def9b0cf42f98982f2b05f34ab0d18c332
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-05-15 02:41:40 +0700

    add slug page

---
+++ pages/projects/_slug.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  div.container.mx-auto.h-full
+    article.pt-6
+      h1 {{ page.title }}
+      nuxt-content(:document="page")
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const page = await $content("projects", params.slug).fetch();
+
+    return { page };
+  }
+};
+</script>
