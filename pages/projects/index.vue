c3ab1adca58cc510c878c7fd1cdf26f86719d3d5
Nathakit Sae-Tan <nathakit.tan@gmail.com>
2021-05-16 01:07:12 +0700

    comment out unused tabs update the projects section

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -8,38 +8,46 @@
     div.container.mx-auto
       div.px-10.py-6
         div.tab.flex.space-x-4.pb-2.text-sm
-          div.flex.items-center.justify-center.p-2.cursor-pointer.rounded-md.select-none(
-            class="hover:bg-gray-200"
-          ) All Projects
-          div.flex.items-center.justify-center.p-2.cursor-pointer.rounded-md.select-none(
-            class="hover:bg-gray-200"
-          ) Plugins
-          div.flex.items-center.justify-center.p-2.cursor-pointer.rounded-md.select-none(
-            class="hover:bg-gray-200"
-          ) Experiments
+          div.flex.items-center.justify-center.p-2 All Projects
+          //- div.flex.items-center.justify-center.p-2.cursor-pointer.rounded-md.select-none(
+          //-   class=""
+          //- ) Plugins
+          //- div.flex.items-center.justify-center.p-2.cursor-pointer.rounded-md.select-none(
+          //-   class="hover:bg-gray-200"
+          //- ) Experiments
         div.border-b
     //- projects
     div.container.mx-auto
       div.px-10.pb-10.pt-2.grid.grid-cols-2.gap-8
         template(v-for="post in projects")
           div
-            img.pb-4.object-cover.w-full.h-auto(
-              src="https://via.placeholder.com/720x405"
-            )
-            h2.font-bold.text-lg {{ post.title }}
-            p.text-sm {{ post.subtitle }}
+            a.cursor-pointer(:href="post.link" target="_blank")
+              img.object-cover.w-full.h-auto.rounded(
+                :src="require(`~/assets/images/projects/${post.hero_img}`)"
+              )
+            h2.font-bold.text-lg.pt-3 {{ post.title }}
+            p.text-sm.pb-2 {{ post.subtitle }}
+            //- p.text-sm.pb-2 {{ post.date }}
+            div.rounded-md.bg-gray-200.w-min
+              div.flex.items-center.justify-center.px-2.py-1.text-sm
+                | {{ capitalize(post.tag) }}
 </template>
 
 <script>
 export default {
   async asyncData({ $content, params }) {
     const projects = await $content("projects", params.slug)
-      .only(["title", "subtitle", "img", "slug"])
-      .sortBy("createdAt", "asc")
+      .only(["title", "subtitle", "hero_img", "slug", "link", "tag", "date"])
+      .sortBy("date", "desc")
       .fetch();
     return {
       projects
     };
+  },
+  methods: {
+    capitalize(text) {
+      return text.charAt(0).toUpperCase() + text.slice(1);
+    }
   }
 };
 </script>
